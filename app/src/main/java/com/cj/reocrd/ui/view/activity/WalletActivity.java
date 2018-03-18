c29296730c7aee82055cb2d31def68049b4747cd
576204520 <576204520@qq.com>
2018-03-18 20:15:34 +0800

    111

--- app/src/main/java/com/cj/reocrd/ui/view/activity/WalletActivity.java
+++ app/src/main/java/com/cj/reocrd/ui/view/activity/WalletActivity.java
@@ -7,10 +7,10 @@ import com.cj.reocrd.ui.base.BaseActivity;
  * Created by Administrator on 2018/3/18.
  */
 
-public class RefundActivity extends BaseActivity {
+public class WalletActivity extends BaseActivity {
     @Override
     public int getLayoutId() {
-        return R.layout.activity_refund;
+        return R.layout.activity_wallet;
     }
 
     @Override
